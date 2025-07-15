8c12208249430fbdc7457bbc6a2753a62991c092
jayapriya-18 <134153984+jayapriya-18@users.noreply.github.com>
2025-07-15 14:17:56 +0530

    Add files via upload

---
+++ basicsOfJava/LearnJava.java
@@ -0,0 +1,14 @@
+package basicsOfJava;
+
+public class LearnJava {
+// Eligible age for vote
+	public static void main(String[] args) {
+		int age = 18;
+		if(age >= 18) {
+			System.out.println("Eligible for vote");
+		}
+		else {
+			System.out.println("Not Eligible for Vote");
+		}
+	}
+}
---
+++ basicsOfJava/PrintYourName.java
@@ -0,0 +1,11 @@
+package basicsOfJava;
+
+public class PrintYourName {
+
+	public static void main(String[] args) {
+		// Print your name
+		System.out.println("My Name is Jayapriya");
+
+	}
+
+}
